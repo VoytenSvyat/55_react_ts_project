040d4169f6b2bb1ad2197e2f299e027ebd82ae2a
VoytenSvyat <voytensvyat@gmail.com>
2025-04-03 13:06:17 +0200

    lesson13_practice

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 // import Lesson10 from "./lessons/Lesson10/Lesson10";
 // import Lesson11 from "./lessons/Lesson11/Lesson11";
 // import Lesson12 from "./lessons/Lesson12/Lesson12";
+import Lesson13 from "./lessons/Lesson13/Lesson13";
 
 //Homeworks imports:
 // import Homework06 from "./homeworks/Homework06";
@@ -20,7 +21,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 // import Homework09 from "./homeworks/Homework09/Homework09";
 // import Homework10 from "./homeworks/Homework10/Homework10";
 // import Homework11 from "./homeworks/Homework11/Homework11";
-import Homework12 from "./homeworks/Homework12/Homework12";
+// import Homework12 from "./homeworks/Homework12/Homework12";
 
 function App() {
 
@@ -42,7 +43,8 @@ function App() {
       {/* <Consultation05 /> */}
       {/* <Homework11/> */}
       {/* <Lesson12/> */}
-      <Homework12/>
+      {/* <Homework12/> */}
+      <Lesson13/>
     </>
   )
 }
---
+++ src/lessons/Lesson13/styles.ts
@@ -0,0 +1,33 @@
+import styled from "@emotion/styled";
+
+export const Lesson13Container = styled.div`
+   display: flex;
+flex-direction: column;
+align-items: center;
+gap: 40px;
+flex: 1;
+padding: 60px;
+background-color: rgb(245, 249, 250);
+/* overflow-y: scroll;
+height: 700px;  */
+border: 1px solid #ccc;
+
+`;
+
+export const AnimalImage = styled.img`
+    max-width: 80%;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+`;
+
+export const LoadingText = styled.p`
+    font-size: 18px;
+    color: #555;
+`;
+
+export const Error = styled.div`
+    font-size: 28px;
+    color: red;
+    font-weight: bold;
+`
