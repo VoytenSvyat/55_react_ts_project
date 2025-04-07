5c860e94133363c2ce4fa00ec04a136b4dae7c7a
VoytenSvyat <voytensvyat@gmail.com>
2025-04-07 09:45:01 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 // import Lesson10 from "./lessons/Lesson10/Lesson10";
 // import Lesson11 from "./lessons/Lesson11/Lesson11";
 // import Lesson12 from "./lessons/Lesson12/Lesson12";
+import Lesson13 from "./lessons/Lesson13/Lesson13";
 
 
 
@@ -32,7 +33,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 // import Homework08 from "./homeworks/Homework08/Homework08";
 // import Homework09 from "./homeworks/Homework09/Homework09";
 
-
+>>>>>>> main
 >>>>>>> main
 
 function App() {
@@ -51,21 +52,6 @@ function App() {
       {/* <Homework09/> */}
       {/* <Lesson10/>
       <Homework10/> */}
-   
-
-     
-
-
-
-
-
-
-
-
-
-
-
-      
 
 >>>>>>> main
 >>>>>>> main
